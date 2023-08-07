bbd9d6feff537fd106e7afa43452aa8dc0ded34c
Kyo9xtb <phamvanthien307@gmail.com>
2023-08-07 17:05:04 +0700

    Cấu hình môi trường làm việc

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -4,6 +4,7 @@ function DefaultLayout({children}) {
     return ( 
         <React.Fragment>
             <h1>DefaultLayout</h1>
+            <h2>sss</h2>
             {children}
         </React.Fragment>
      );
