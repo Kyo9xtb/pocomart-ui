cbf2db0b2f2c5d42b6e9abb970252d692cf39d8e
Kyo9xtb <phamvanthien307@gmail.com>
2023-08-22 11:40:03 +0700

    header-ui

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -4,7 +4,6 @@ function DefaultLayout({children}) {
     return ( 
         <React.Fragment>
             <h1>DefaultLayout</h1>
-            <h2>sss</h2>
             {children}
         </React.Fragment>
      );
